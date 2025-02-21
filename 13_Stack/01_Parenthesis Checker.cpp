ea5c8531725c7286ceb3596bf0cf06656b539772
imnilesh18 <nileshshrivastava7737@gmail.com>
2025-02-21 23:06:24 +0530

    Parenthesis Checker

--- 13_Stack/01_Parenthesis Checker.cpp
+++ 13_Stack/01_Parenthesis Checker.cpp
@@ -30,7 +30,6 @@
  * s[i] ∈ {'{', '}', '(', ')', '[', ']'}
  */
 
-
 // Approach: Use a stack to validate balanced brackets by pushing opening brackets and matching them with closing brackets.
 // TC: O(n) because we traverse each character once.
 // SC: O(n) in the worst-case scenario when all characters are opening brackets.
